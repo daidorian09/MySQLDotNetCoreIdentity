b0af3c729c870f8414322d33ec9fd976b38a7043
daidorian09 <yigitat1992@gmail.com>
2017-10-26 17:24:54 +0300

    Add project files.

---
+++ Mnb.Authentication.Services/Identity/TestUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Mnb.Authentication.Services.Identity
+{
+    public class TestUser : IdentityUser
+    {
+        public string TestDescrition { get; set; }
+    }
+}
---
+++ Mnb.Authentication.Services/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Mnb.Authentication.Services.DAL;
+using Mnb.Authentication.Services.Identity;
+
+namespace Mnb.Authentication.Services
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<TestAuthDBContext>(options =>
+                                                     options.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
+                                                     mySQLOptions => mySQLOptions.MigrationsAssembly("Mnb.Authentication.Services")));
+
+            services.AddIdentity<TestUser, TestRole>()
+                    .AddEntityFrameworkStores<TestAuthDBContext>()
+                    .AddDefaultTokenProviders();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
